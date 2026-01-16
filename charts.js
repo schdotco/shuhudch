7c88aa952c5ebb8d43a5de49c67129caf627b2e1
Puskesmas Padasua <schdotco@gmail.com>
2026-01-16 19:55:14 +0700

    Update charts.js

--- charts.js
+++ charts.js
@@ -1,19 +1,50 @@
 const ctx = document.getElementById('kpspChart');
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Sesuai', 'Meragukan', 'Penyimpangan'],
-    datasets: [{
-      label: 'Jumlah Anak',
-      data: [35, 8, 4], // NANTI DIAMBIL DARI SPREADSHEET
-      backgroundColor: ['#198754','#ffc107','#dc3545']
-    }]
-  },
-  options: {
-    responsive: true,
-    plugins:{
-      legend:{display:false}
+let chartKPSP;
+
+// DATA AGREGAT
+let kategori = {
+  sesuai: 0,
+  meragukan: 0,
+  penyimpangan: 0
+};
+
+// PANGGIL SETELAH DATA FIREBASE TERBACA
+function updateChartKPSP() {
+
+  if(chartKPSP) chartKPSP.destroy();
+
+  chartKPSP = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Sesuai', 'Meragukan', 'Penyimpangan'],
+      datasets: [{
+        label: 'Jumlah Anak',
+        data: [
+          kategori.sesuai,
+          kategori.meragukan,
+          kategori.penyimpangan
+        ],
+        backgroundColor: ['#198754','#ffc107','#dc3545'],
+        borderRadius: 8
+      }]
+    },
+    options: {
+      responsive: true,
+      plugins:{
+        legend:{display:false},
+        tooltip:{
+          callbacks:{
+            label:(ctx)=> ` ${ctx.raw} anak`
+          }
+        }
+      },
+      scales:{
+        y:{
+          beginAtZero:true,
+          ticks:{precision:0}
+        }
+      }
     }
-  }
-});
+  });
+}
